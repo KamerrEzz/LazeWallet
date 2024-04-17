8e6919306f7940ade52308185134ce91e5619279
Kamerr Ezz <kamerr_oficial@hotmail.com>
2024-04-16 21:53:30 -0600

    feat(api): jwt add

--- api/apps/api/src/auth/auth.module.ts
+++ api/apps/api/src/auth/auth.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { UsersModule } from '../users/users.module';
+import { LocalStrategy } from './local.strategy';
+import { PassportModule } from '@nestjs/passport';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
 
 @Module({
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '20h' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy],
+  exports: [AuthService]
 })
 export class AuthModule {}
